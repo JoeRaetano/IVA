3e0cb248fb87c0f84eefa356187041c917f1d11b
JoshFagan <jfagan2@vols.utk.edu>
2015-07-07 12:19:23 -0400

    Removes unneeded code from previous tentative attempt at implementing modular pid and function information

--- server/api/collection/index.js
+++ server/api/collection/index.js
@@ -10,7 +10,6 @@ var router = express.Router();
 
 router.get('/', controller.index);
 router.get('/:id', controller.show);
-router.get('/:id/:id', controller.show);
 router.post('/', controller.create);
 router.put('/:id', controller.update);
 router.put('/pid/:id', controller.updatePid);
