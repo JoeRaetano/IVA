247d4631fe0bfffb984480aabf17b33edccfa5c2
JoshFagan <jfagan2@vols.utk.edu>
2015-07-09 14:06:31 -0400

    Adds route to get functions associated to specific pid

--- server/api/pid/index.js
+++ server/api/pid/index.js
@@ -7,6 +7,7 @@ var router = express.Router();
 
 router.get('/', controller.index);
 router.get('/:id', controller.show);
+router.get('/func/:id', controller.showFuncsForPid);
 router.post('/', controller.create);
 router.put('/:id', controller.update);
 router.put('/func/:id', controller.updateFunc);
