1403a47a537d7870dcfaa8835d75f3052c649fe2
JoshFagan <jfagan2@vols.utk.edu>
2015-08-03 13:37:02 -0400

    Adds comments

--- server/api/pid/index.js
+++ server/api/pid/index.js
@@ -5,13 +5,12 @@ var controller = require('./pid.controller');
 
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.get('/func/:id', controller.showFuncsForPid);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.put('/func/:id', controller.updateFunc);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+router.get('/', controller.index);                  // Returns all of the PID documents in the PID Collection
+router.get('/:id', controller.show);                // Returns the PID with the supplied document id
+router.get('/vehicle/:id', controller.getVehicles); // Returns all PIDs that belong to the specified Vehicle document id
+router.post('/', controller.create);                // Creates a new PID document in the PID Collection
+router.put('/:id', controller.update);              // Updates information stored in the PID with supplied document id
+router.patch('/:id', controller.update);            // Updates information stored in the PID with supplied document id
+router.delete('/:id', controller.destroy);          // Deletes the Vehicle with the supplied document id
 
 module.exports = router;
