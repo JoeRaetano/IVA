9e9ac56588f9a7eb724014e66d00fa699530df5b
JoshFagan <jfagan2@vols.utk.edu>
2015-08-03 13:52:50 -0400

    Adds comments

--- server/api/vehicle/index.js
+++ server/api/vehicle/index.js
@@ -8,14 +8,12 @@ var controller = require('./vehicle.controller');
 
 var router = express.Router();
 
-router.get('/', controller.index);
-router.get('/:id', controller.show);
-router.get('/query/:term', controller.query);
-router.get('/pid/:id', controller.showPidsForVehicle);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.put('/pid/:id', controller.updatePid);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+router.get('/', controller.index);            // Returns all of the Vehicle documents in the Vehicle Collection.
+router.get('/:id', controller.show);          // Returns the Vehicle with the supplied document id
+router.get('/query/:term', controller.query); //
+router.post('/', controller.create);          // Creates a new Vehicle document in the Vehicle Collection
+router.put('/:id', controller.update);        // Updates information stored in the Vehicle with the supplied document id
+router.patch('/:id', controller.update);      // Updates information stored in the Vehicle with the supplied document id
+router.delete('/:id', controller.destroy);    // Deletes the Vehicle with the supplied document id
 
 module.exports = router;
