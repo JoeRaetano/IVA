3ce4045210866c0732d7e142fe55068d14901be7
JoshFagan <jfagan2@vols.utk.edu>
2015-08-10 09:34:15 -0400

    Adds routes for querying functions and getting the list of tags for all functions

--- server/api/function/index.js
+++ server/api/function/index.js
@@ -6,8 +6,10 @@ var controller = require('./function.controller');
 var router = express.Router();
 
 router.get('/', controller.index);          // Returns all of the Function documents in the Function Collection
-router.get('/:id', controller.show);        // Returns the PID with the supplied document id
 router.get('/pid/:id', controller.getPids); // Returns all Functions that belong to the specified PID document id
+router.get('/query/:expression', controller.query);
+router.get('/tags', controller.getTags);
+router.get('/:id', controller.show);        // Returns the PID with the supplied document id
 router.post('/', controller.create);        // Creates a new PID document in the PID Collection
 router.put('/:id', controller.update);      // Updates information stored in the PID with supplied document id
 router.patch('/:id', controller.update);    // Updates information stored in the PID with supplied document id
